5126b1b9378b9bb1b5c49fc47f0b4fb7c8578294
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-04-12 21:32:30 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -172,7 +172,7 @@ async def play(_, message: Message):
         await generate_cover(requested_by, title, views, duration, thumbnail)     
         file_path = await converter.convert(youtube.download(url))
     else:
-        await lel.edit("🔎 **SABAR SEDANG DICARI LAGUMU!**")
+        await lel.edit("😑 **SABAR SEDANG DICARI LAGUMU!**")
         sender_id = message.from_user.id
         user_id = message.from_user.id
         sender_name = message.from_user.first_name
@@ -183,7 +183,7 @@ async def play(_, message: Message):
         for i in message.command[1:]:
             query += ' ' + str(i)
         print(query)
-        await lel.edit("🎵 **SABAR LAGUMU SEDANG DIPROSES, HARAP TUNGGU SEBENTAR!**")
+        await lel.edit("😂 **SABAR LAGUMU SEDANG DIPROSES, HARAP TUNGGU SEBENTAR!**")
         ydl_opts = {"format": "bestaudio[ext=m4a]"}
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
@@ -232,7 +232,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="✨ **LAGUMU SUDAH MULAI!** Lagu Ini Request-an By {} via Savira Music! 😝✨".format(
+        caption="✨ **LAGUMU SUDAH MULAI!** Lagu Ini Request-an By {} via Savira Music! 😝".format(
         message.from_user.mention()
         ),
     )
