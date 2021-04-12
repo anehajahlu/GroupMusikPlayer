bfbaf92b969852db737faba87f45ca61794128ed
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-04-12 19:42:27 +0700

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -23,7 +23,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("MENCARI LAGUMU!....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
