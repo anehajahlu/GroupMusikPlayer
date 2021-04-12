79f2c6404d8ba82ed9035d0a7788a2f843cf3f9f
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-04-12 20:54:39 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -18,10 +18,10 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text("❗ Nothing is playing!")
+        await message.reply_text("❗ TIDAK BISA DI MULAI PAKSA!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text("▶️ LAGUMU SUDAH DIPAUSE!/DIHENTIKAN SEMENTARA!")
+        await message.reply_text("😏 LAGUMU SUDAH DIPAUSE!/DIHENTIKAN! SEMENTARA!")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -33,10 +33,10 @@ async def resume(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
     ):
-        await message.reply_text("❗ Nothing is paused!")
+        await message.reply_text("❗ TIDAK BISA DI STOP PAKSA!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text("⏸ LAGUMU SUDAH DIMULAI KEMBALI!")
+        await message.reply_text("😅 LAGUMU SUDAH DIMULAI KEMBALI!")
 
 
 @Client.on_message(command("stop") & other_filters)
@@ -44,7 +44,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is streaming!")
+        await message.reply_text("❗ TIDAK BISA STOP STREAMING PAKSA!")
     else:
         try:
             callsmusic.queues.clear(message.chat.id)
@@ -52,7 +52,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("❌ LAGUMU SUDAH DIHENTIKAN/DISTOP!")
+        await message.reply_text("🥺 LAGUMU SUDAH DIHENTIKAN!/DISTOP!")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -60,7 +60,7 @@ async def stop(_, message: Message):
 @authorized_users_only
 async def skip(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is playing to skip!")
+        await message.reply_text("❗ TIDAK BISA DI SKIP PAKSA!")
     else:
         callsmusic.queues.task_done(message.chat.id)
 
@@ -72,4 +72,4 @@ async def skip(_, message: Message):
                 callsmusic.queues.get(message.chat.id)["file"]
             )
 
-        await message.reply_text("➡️ OKEYY,LANJUT KE LAGU SELANJUTNYA!")
+        await message.reply_text("🥰 OKEYY,LANJUT KE LAGU SELANJUTNYA!")
